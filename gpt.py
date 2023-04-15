a37126515f334165fea676c36fb0440c79702724
Michael S <mstang822@gmail.com>
2023-04-15 02:26:22 -0500

    Add files via upload

---
+++ gpt.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = "KEY"
+
+def main(prompt):
+    # Set the model and prompt
+    model_engine = "text-davinci-002"
+
+    # Set the maximum number of tokens to generate in the response
+    max_tokens = 200
+
+    # Generate a response
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=0.5,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+
+    # Print the response
+    return completion.choices[0].text
