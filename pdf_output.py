8ab0b488d2d49e76ae143f35c5cda20f558f7cd4
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2023-04-15 17:02:41 -0500

    new pdfs

--- pdf_output.py
+++ pdf_output.py
@@ -5,16 +5,15 @@ def generate_pdf(note):
     #Creates a PDF object and sets its font and starting coordinates.
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_xy(0, 0)
-    pdf.set_font('Times', 'UB', 16)
+    pdf.set_xy(0, 10)
 
     #Adds a header saying the following will be the condensed notes. 
+    pdf.set_font('Times', 'UB', 16)
     pdf.cell(ln=0, h=6, align='C', w=0, txt='Generated Notes', border = 0)
 
     #Adds the summary generated from ChatGPT to the PDF, which comes from the "note" object.
     pdf.set_font('Times', '', 12)
-    #pdf.set_y(pdf.get_y()+10)
-
+    pdf.set_xy(10,10)
 
     note.summary = fix_apostrophe(note.summary)
     pdf.multi_cell(w = 0, h = 6, txt = note.summary, border = 0, align = 'L', fill = False)
@@ -26,8 +25,8 @@ def generate_pdf(note):
     pdf.cell(ln=0, h=6, align='C', w=0, txt='Original Audio Transcript', border = 0)
 
     #Adds the full transcription from the google cloud api into the pdf.
+    pdf.set_xy(10, pdf.get_y()+10)
     pdf.set_font('Times', '', 12)
-    #pdf.set_xy(10, pdf.get_y()+10)
     
     note.transcription = fix_apostrophe(note.transcription)
     pdf.multi_cell(w = 0, h = 6, txt = note.transcription, border = 0, align = 'L', fill = False)
