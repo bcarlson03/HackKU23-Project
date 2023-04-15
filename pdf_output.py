b79068694fc563788627fab64d0c0d60f52fefe1
Blake Carlson <blake.carlson@ku.edu>
2023-04-15 15:49:41 -0500

    Update pdf_output.py
    Fixes formatting issues with the generation of the pdf. For example, it makes the headers more distinct and increases the spacing between lines.

--- pdf_output.py
+++ pdf_output.py
@@ -1,4 +1,4 @@
-#What needs to be imported into "output".
+#Imports FPDF so it has the functionality to create a pdf file.
 from fpdf import FPDF
 
 def generate_pdf(note):
@@ -6,33 +6,39 @@ def generate_pdf(note):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_xy(0, 20)
-    pdf.set_font('Times', 'B', 12.0)
+    pdf.set_font('Times', '', 12)
 
-    
     #Adds a header saying the following will be the condensed notes. 
-    pdf.cell(ln=0, h=5.0, align='C', w=0, txt='Generated Notes', border = 0)
+    pdf.set_font('Times', 'UB', 16)
+    pdf.cell(ln=0, h=6, align='C', w=0, txt='Generated Notes', border = 0)
 
-    #Adds the summary generated from ChatGPT to the PDF.
-    summary_string = ''
-    for line in note.summary:
-        summary_string += line + '\n'
-    pdf.set_xy(10, 30)
-    pdf.multi_cell(w = 0, h = 5, txt = note.summary, border = 0, align = 'L', fill = False)
-
-    pdf.set_y(pdf.get_y()+10)
-    #Adds a line separating the summary with the original transcript.
-    pdf.cell(ln=0, h=5.0, align='C', w=0, txt='Original Audio Transcript', border = 0)
-
-    #Adds the full transcription from the google cloud api into the pdf. 
-    transcription_string = ''
-    for line in note.transcription:
-        transcription_string += line + '\n'
+    #Adds the summary generated from ChatGPT to the PDF, which comes from the "note" object.
+    pdf.set_font('Times', '', 12)
     pdf.set_xy(10, pdf.get_y()+10)
+
+    note.summary = fix_apostrophe(note.summary)
+    pdf.multi_cell(w = 0, h = 6, txt = note.summary, border = 0, align = 'L', fill = False)
+
+    #Adds a line separating the summary with the original transcript.
+    pdf.set_y(pdf.get_y()+10)
+    pdf.set_font('Times', 'UB', 16)
+
+    pdf.cell(ln=0, h=6, align='C', w=0, txt='Original Audio Transcript', border = 0)
+
+    #Adds the full transcription from the google cloud api into the pdf.
+    pdf.set_xy(10, pdf.get_y()+10)
+    pdf.set_font('Times', '', 12)
     
-    pdf.multi_cell(w = 0, h = 5, txt = note.transcription, border = 0, align = 'L', fill = False)
+    note.transcription = fix_apostrophe(note.transcription)
+    pdf.multi_cell(w = 0, h = 6, txt = note.transcription, border = 0, align = 'L', fill = False)
 
     #Creates the pdf file with the same name as the mp3 file. 
     pdf.output(f"notes/{note.name}.pdf", 'F')
 
+def fix_apostrophe(body_information):
+    #Fixes ChatGPT's weird apostrophe formatting so apostrophes appears normal when viewed in the pdf. 
+    return body_information.replace(r"\'", "'")
+
 def main(note):
+    #main() is ran in main.py to create the pdf with the information from note.
     generate_pdf(note)
