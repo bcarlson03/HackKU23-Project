cdfbe2d234f09b752698c5d7bcca67285a6b32e0
Michael S <mstang822@gmail.com>
2023-04-16 07:56:00 -0500

    Add files via upload

--- note_maker.py
+++ note_maker.py
@@ -4,12 +4,12 @@ import gpt
 def main(note):
     
     # This is the initial prompt that the model will use to generate the response
-    prompt = "Please take the following transcript and turn it into a note sheet for a person who can't take notes. It should include all of the main points and should include ALL of the important information in the transcript below. Do NOT leave out anything important. Make sure to separate various section with headers to make sure it's not just a list of bullet points."
+    prompt = "Please take the following transcript and turn it into a note sheet for a person who can't take notes. It should include all of the main points and should include ALL of the important information in the transcript below. Do NOT leave out anything important. Make sure to separate various section with headers to make sure it's not just a list of bullet points, and ensure you are not completing the transcript, just make a broad summary: "
 
     # Adds the transcript
     prompt += note.transcription
 
     # Gets GPT's response
     response = gpt.main(prompt)
-
+    print("RESPONSE= " + response)
     note.summary = response
